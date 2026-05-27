742c298b99893ef812e54e807249230cfa438081
KMB <informatique@centrelago.com>
2026-05-27 11:44:36 +0200

    - replace color with style

--- views/menu_display.py
+++ views/menu_display.py
@@ -9,26 +9,26 @@ if TYPE_CHECKING:
     from models.menu_item import MenuItem
 
 
-class ColorPicker:
-    exit_color = "bold red"
-    back_color = "bold yellow"
-    navigation_color = "bold cyan"
-    action_color = "bold green"
-    invalid_color = "red"
-    input_awaiting_color = "green"
+class StylePicker:
+    exit_style = "bold red"
+    back_style = "bold yellow"
+    navigation_style = "bold cyan"
+    action_style = "bold green"
+    invalid_style = "red"
+    input_awaiting_style = "green"
 
     @classmethod
-    def pickup_color(cls, key: str, item: MenuItem) -> str:
+    def pickup_style(cls, key: str, item: MenuItem) -> str:
         if key == EXIT_SHORTCUT:
-            return cls.exit_color
+            return cls.exit_style
 
         if key == BACK_SHORTCUT:
-            return cls.back_color
+            return cls.back_style
 
         if not item.child_items:
-            return cls.action_color
+            return cls.action_style
 
-        return cls.navigation_color
+        return cls.navigation_style
 
 
 class MenuDisplay:
@@ -36,12 +36,12 @@ class MenuDisplay:
     def __init__(self) -> None:
         self.console = Console()
 
-    def color_string(self, string: str, color: str):
-        return f"[{color}]{string}[/{color}]"
+    def style_string(self, string: str, style: str) -> str:
+        return f"[{style}]{string}[/]"
 
     def format_item_display(self, key: str, item: MenuItem) -> str:
-        shortcut_color = ColorPicker.pickup_color(key=key, item=item)
-        return f"{self.color_string(string=key,color=shortcut_color)} - {item.title}"
+        shortcut_style = StylePicker.pickup_style(key=key, item=item)
+        return f"{self.style_string(string=key,style=shortcut_style)} - {item.title}"
 
     def display_items(self, items: dict[str, MenuItem]) -> None:
         for key, item in items.items():
@@ -54,12 +54,12 @@ class MenuDisplay:
 
     def display_invalid_choice(self) -> None:
         message = "Please select available choices"
-        color = ColorPicker.invalid_color
-        self.console.print(self.color_string(string=message, color=color))
+        style = StylePicker.invalid_style
+        self.console.print(self.style_string(string=message, style=style))
 
     def display_choice_asking(self) -> str:
         message = "Sélectionner une action : "
-        color = ColorPicker.input_awaiting_color
+        style = StylePicker.input_awaiting_style
         return self.console.input(
-            self.color_string(string=message, color=color)
+            self.style_string(string=message, style=style)
         ).upper()
