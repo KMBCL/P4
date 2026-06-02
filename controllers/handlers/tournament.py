6b7152a07ee23bf44fc72619a0d3bca2529c166d
KMB <informatique@centrelago.com>
2026-06-02 22:03:58 +0200

    - moved prompthandler to handler files

---
+++ controllers/handlers/tournament.py
@@ -0,0 +1,48 @@
+from views.tournament import TournamentView
+
+from controllers.shortcuts.tournament import TournamentShortcuts
+
+from controllers.handlers.date_prompt import DatePromptHandler
+from controllers.handlers.action_prompt import ActionPromptHandler
+
+from models.tournament import Tournament
+
+
+class TournamentPromptHandler:
+
+    def __init__(self, view: TournamentView) -> None:
+        self.view = view
+        self.date_prompt_handler = DatePromptHandler[Tournament](self.view)
+        self.action_prompt_handler = ActionPromptHandler[Tournament](self.view)
+
+    def prompt_action(self) -> str:
+        return self.action_prompt_handler.prompt_action(
+            action_shortcuts=TournamentShortcuts
+        )
+
+    def prompt_name(self) -> str:
+        return self.view.prompt_name()
+
+    def prompt_place(self) -> str:
+        return self.view.prompt_place()
+
+    def prompt_description(self) -> str:
+        return self.view.prompt_description()
+
+    def prompt_round_count(self) -> str:
+        return self.view.prompt_round_count()
+
+    def prompt_start_date(self) -> str:
+        return self.date_prompt_handler.prompt_date(self.view.prompt_start_date)
+
+    def prompt_end_date(self) -> str:
+        return self.date_prompt_handler.prompt_date(self.view.prompt_end_date)
+
+
+class TournamentRenderHandler:
+
+    def __init__(self, view: TournamentView) -> None:
+        self.view = view
+
+    def render_tournaments(self, tournaments: list[Tournament]):
+        self.view.render_models(tournaments)
--- controllers/player.py
+++ controllers/player.py
@@ -1,60 +1,11 @@
-from __future__ import annotations
-
 from views.player import PlayerView
-
-from controllers.validators.chess_id import ChessIDValidator
 
 from controllers.shortcuts.player import PlayerShortcuts
 
-from controllers.handlers.date_prompt import DatePromptHandler
-from controllers.handlers.action_prompt import ActionPromptHandler
+from controllers.handlers.player import PlayerPromptHandler, PlayerRenderController
 
 from models.player import Player, PlayerInputData
 from repository.player import PlayerRepository
-
-
-class PlayerPromptHandler:
-
-    def __init__(self, view: PlayerView) -> None:
-        self.view = view
-        self.chess_id_validator = ChessIDValidator()
-
-        self.date_prompt_handler = DatePromptHandler[Player](self.view)
-        self.action_prompt_handler = ActionPromptHandler[Player](self.view)
-
-    def prompt_action(self) -> str:
-        return self.action_prompt_handler.prompt_action(
-            action_shortcuts=PlayerShortcuts
-        )
-
-    def prompt_chess_id(self) -> str:
-        while True:
-            user_input = self.view.prompt_chess_id()
-
-            user_input_result = self.chess_id_validator.validate_chess_id(user_input)
-            if not user_input_result:
-                self.view.render_invalid_input(user_input_result.required_reason)
-                continue
-
-            return user_input
-
-    def prompt_last_name(self) -> str:
-        return self.view.prompt_last_name()
-
-    def prompt_first_name(self) -> str:
-        return self.view.prompt_first_name()
-
-    def prompt_birthdate(self) -> str:
-        return self.date_prompt_handler.prompt_date(self.view.prompt_birthdate)
-
-
-class PlayerRenderController:
-
-    def __init__(self, view: PlayerView) -> None:
-        self.view = view
-
-    def render_players(self, players: list[Player]):
-        self.view.render_models(players)
 
 
 class PlayerController:
